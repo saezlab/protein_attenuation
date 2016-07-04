eb7bee9b184c74ae3c9305291e99380068fca990
Emanuel Goncalves <emanuelvgoncalves@gmail.com>
2016-07-04 20:38:49 +0100

    Protein-pairs associations with survival

--- src/cptac/interactions/corum_survival.py
+++ src/cptac/interactions/corum_survival.py
@@ -2,6 +2,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from cptac import wd
+import statsmodels.api as sm
 from sklearn.mixture.gmm import GMM
 from lifelines import KaplanMeierFitter
 from matplotlib.gridspec import GridSpec
@@ -12,74 +13,105 @@ from statsmodels.stats.multitest import multipletests
 
 # -- Imports
 # protein complexes scores
-corum = read_csv('%s/tables/corum_scores.csv' % wd, index_col=0)
-samples = {'-'.join(i.split('-')[:4])[:-1].upper() for i in corum}
-corum = corum[corum.count(1) > (corum.shape[1] * .5)]
+pancan = read_csv('%s/tables/pancan_normalised.csv' % wd, index_col=0)
+pancan = pancan[pancan.count(1) > (pancan.shape[1] * .5)]
+samples = {'-'.join(i.split('-')[:4])[:-1].upper() for i in pancan}
+print pancan
+
+# correlating pairs
+p_pairs = read_csv('%s/tables/top_correlated_protein_pairs.csv' % wd)
+p_pairs = p_pairs[p_pairs['score'] > .5]
+print p_pairs
 
 # Clinical data
 clinical = read_csv('%s/data/clinical_data.tsv' % wd, sep='\t', index_col=0).ix[samples, ['DAYS_TO_LAST_FOLLOWUP', 'VITAL_STATUS']]
 clinical['VITAL_STATUS'] = [0 if i == 'Alive' else 1 for i in clinical['VITAL_STATUS']]
+print clinical
 
 
 # --
-# c = 'APP-TIMM23 complex'
-def complex_gmm(c):
-    # -- Get complex scores
-    complex_scores = corum.ix[[c]].T.dropna()
+# p1, p2 = 'ACOT2', 'ACOT1'
+def gmm_survival(p1, p2):
+    name = '%s-%s' % (p1, p2)
+    print name
+
+    # -- Residuals
+    # Get protein measurements
+    x = pancan.ix[[p1, p2]].T.dropna()[p1]
+    y = pancan.ix[[p1, p2]].T.dropna()[p2]
+
+    lm = sm.OLS(y, sm.add_constant(x)).fit()
+    residuals = DataFrame(lm.resid)
 
     # -- GMM
-    gmm = GMM(n_components=2).fit(complex_scores)
+    gmm = GMM(n_components=3).fit(residuals)
 
-    group0 = set(complex_scores[np.logical_not(gmm.predict(complex_scores).astype(np.bool))].index)
-    group0_ = {'-'.join(i.split('-')[:4])[:-1].upper() for i in group0}
+    groups = {
+        'low': gmm.means_.argmin(),
+        'high': gmm.means_.argmax(),
+        'neutral': list({0, 1, 2} - {gmm.means_.argmin(), gmm.means_.argmax()})[0]
+    }
 
-    group1 = set(complex_scores[gmm.predict(complex_scores).astype(np.bool)].index)
-    group1_ = {'-'.join(i.split('-')[:4])[:-1].upper() for i in group1}
+    groups = {g: set(residuals[gmm.predict(residuals) == groups[g]].index) for g in groups}
+    groups_ = {g: {'-'.join(i.split('-')[:4])[:-1].upper() for i in groups[g]} for g in groups}
 
-    # -- Logrank test
-    logrank = logrank_test(
-        clinical.ix[group0_, 'DAYS_TO_LAST_FOLLOWUP'], clinical.ix[group1_, 'DAYS_TO_LAST_FOLLOWUP'],
-        clinical.ix[group0_, 'VITAL_STATUS'], clinical.ix[group1_, 'VITAL_STATUS']
-    )
+    logrank = np.nan
 
-    # -- Plot
-    if logrank.p_value < .001:
-        sns.set(style='ticks', font_scale=.75, rc={'axes.linewidth': .3, 'xtick.major.width': .3, 'ytick.major.width': .3})
-        fig, gs = plt.figure(figsize=(7, 3)), GridSpec(1, 2, hspace=.3)
-        palette = {'Group 0': '#808080', 'Group 1': '#e74c3c'}
+    if len(groups['low']) > 3 and len(groups['high']) > 3 and len(groups['neutral']) > 3 and len(residuals) > (.5 * pancan.shape[1]):
+        # -- Logrank test
+        logrank = logrank_test(
+            clinical.ix[groups_['high'], 'DAYS_TO_LAST_FOLLOWUP'], clinical.ix[groups_['low'], 'DAYS_TO_LAST_FOLLOWUP'],
+            clinical.ix[groups_['high'], 'VITAL_STATUS'], clinical.ix[groups_['low'], 'VITAL_STATUS']
+        )
 
-        # Survival curves
-        ax = plt.subplot(gs[0])
+        # -- Plot
+        if logrank.p_value < .001:
+            sns.set(style='ticks', font_scale=.75, rc={'axes.linewidth': .3, 'xtick.major.width': .3, 'ytick.major.width': .3})
+            fig, gs = plt.figure(figsize=(10, 3)), GridSpec(1, 3, hspace=.3)
+            palette = {'high': '#2ecc71', 'neutral': '#808080', 'low': '#e74c3c'}
 
-        kmf = KaplanMeierFitter()
-        for n, g in [('Group 0', group0_), ('Group 1', group1_)]:
-            kmf.fit(clinical.ix[g, 'DAYS_TO_LAST_FOLLOWUP'], event_observed=clinical.ix[g, 'VITAL_STATUS'], label=n)
-            kmf.plot(ax=ax, ci_force_lines=False, color=palette[n])
+            # Survival curves
+            ax = plt.subplot(gs[0])
 
-        ax.set_xlabel('Timeline (days)')
-        ax.set_ylabel('Survival probability')
-        sns.despine(ax=ax)
+            kmf = KaplanMeierFitter()
+            for g in groups:
+                kmf.fit(clinical.ix[groups_[g], 'DAYS_TO_LAST_FOLLOWUP'], event_observed=clinical.ix[groups_[g], 'VITAL_STATUS'], label=g)
+                kmf.plot(ax=ax, ci_force_lines=False, color=palette[g])
 
-        # Histogram
-        ax = plt.subplot(gs[1])
+            ax.set_xlabel('Timeline (days)')
+            ax.set_ylabel('Survival probability')
+            sns.despine(ax=ax)
 
-        sns.distplot(complex_scores.ix[group1], color=palette['Group 1'], kde=False, ax=ax)
-        sns.distplot(complex_scores.ix[group0], color=palette['Group 0'], kde=False, ax=ax)
+            # Histogram
+            ax = plt.subplot(gs[1])
 
-        sns.despine(ax=ax)
-        ax.set_xlabel('Protein complex expression score')
-        ax.set_ylabel('Counts')
+            for g in groups:
+                sns.distplot(residuals.ix[groups[g]], color=palette[g], kde=False, ax=ax)
 
-        # Export
-        fig.suptitle('Complex: %s; p-value: %.2e' % (c, logrank.p_value))
-        plt.savefig('%s/reports/survival_complex_%s.pdf' % (wd, c.replace('/', '_')), bbox_inches='tight')
-        plt.close('all')
+            sns.despine(ax=ax)
+            ax.set_xlabel('Protein-pair residuals')
+            ax.set_ylabel('Counts')
 
-        print 'complex: %s; p-value: %.2e, samples: %d' % (c, logrank.p_value, len(complex_scores))
+            # Scatter
+            ax = plt.subplot(gs[2])
 
-    return {'complex': c, 'pval': logrank.p_value}
+            for g in groups:
+                sns.regplot(x.ix[groups[g]], y.ix[groups[g]], ax=ax, label=g, color=palette[g], fit_reg=False)
 
-corum_gmm = DataFrame({c: complex_gmm(c) for c in corum.index}).T
-corum_gmm['fdr'] = multipletests(corum_gmm['pval'], method='fdr_bh')[1]
-print corum_gmm.sort('fdr')
+            ax.axhline(0, ls='--', lw=0.1, c='black', alpha=.3)
+            ax.axvline(0, ls='--', lw=0.1, c='black', alpha=.3)
+            sns.despine(ax=ax)
+
+            # Export
+            # fig.suptitle('%s; p-value: %.2e' % (name, logrank.p_value))
+            plt.savefig('%s/reports/survival_complex_%s.pdf' % (wd, name), bbox_inches='tight')
+            plt.close('all')
+
+            print 'interaction: %s; p-value: %.2e, samples: %d' % (name, logrank.p_value, len(residuals))
+
+        return {'pval': logrank.p_value}
+
+p_survival = DataFrame({'%s - %s' % (p1, p2): gmm_survival(p1, p2) for p1, p2 in p_pairs[['p1', 'p2']].values}).T.dropna()
+p_survival['fdr'] = multipletests(p_survival['pval'], method='fdr_bh')[1]
+print p_survival.sort('fdr')
 
