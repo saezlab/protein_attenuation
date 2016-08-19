b29723e86ab0103a91bdef568c884b663abc4e30
Emanuel Goncalves <emanuelvgoncalves@gmail.com>
2016-08-19 10:39:38 +0100

    clinical data re-assembled

---
+++ src/cptac/preprocess/assemble_clinical.py
@@ -0,0 +1,37 @@
+import os
+import numpy as np
+from pandas import read_csv, concat
+
+# Source data folder
+dwd = '/Users/emanuel/Projects/data/tcga/clinical/'
+
+# Read directories
+dirs = [i for i in os.listdir(dwd) if i.startswith('gdac.broadinstitute.')]
+print len(dirs)
+
+# Clinical data files
+files = ['%s/%s/%s.merged_only_clinical_clin_format.txt' % (dwd, d, d.split('_')[1].split('.')[0]) for d in dirs]
+
+# Read tables
+dfs = []
+for f in files:
+    df = read_csv(f, sep='\t', index_col=0)
+    df.ix['days_to_last_followup'] = df.ix[[i for i in df.index if 'days_to_last_followup' in i]].max()
+    df.ix['days_to_death'] = df.ix[[i for i in df.index if 'days_to_death' in i]].max()
+    df.ix['patient.bcr_patient_barcode'] = [i.upper() for i in df.ix['patient.bcr_patient_barcode']]
+    dfs.append(df)
+
+print len(dfs)
+
+# Concatenate
+df = concat(dfs, axis=1, join='inner').T.set_index('patient.bcr_patient_barcode')
+print df.shape
+
+# Annotate
+df = df.dropna(subset=['patient.vital_status'])
+df['time'] = [dlf if np.isnan(dd) else dd for dd, dlf in df[['days_to_death', 'days_to_last_followup']].values]
+df['status'] = [0 if i.lower() == 'alive' else 1 for i in df['patient.vital_status']]
+print df.shape
+
+# Export
+df.to_csv('%s/tcga_clinical.csv' % dwd)
--- src/cptac/survival/survival_regression.py
+++ src/cptac/survival/survival_regression.py
@@ -1,83 +1,137 @@
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from cptac import wd
+from cptac import wd, palette_cnv_number
 from lifelines import CoxPHFitter
+from lifelines import KaplanMeierFitter
 from lifelines.utils import concordance_index
-from sklearn.preprocessing.imputation import Imputer
+from lifelines.statistics import logrank_test
+from sklearn.linear_model import LinearRegression
+from pymist.utils.map_peptide_sequence import read_uniprot_genename
 from pymist.utils.corumdb import get_complexes_name, get_complexes_dict
 from pandas import DataFrame, Series, read_csv, pivot_table, concat
 
 # -- Imports
-corum_n = get_complexes_name()
+# CNV
+cnv = read_csv('%s/data/tcga_cnv.tsv' % wd, sep='\t', index_col=0)
+print 'cnv', cnv.shape
+
+# Transcriptomics
+transcriptomics = read_csv('%s/data/tcga_rnaseq_corrected_normalised.csv' % wd, index_col=0)
+print 'transcriptomics', transcriptomics.shape
+
+# Proteomics
+proteomics = read_csv('%s/data/cptac_proteomics_corrected_normalised.csv' % wd, index_col=0)
+print 'proteomics', proteomics.shape
+
+# Clinical data
+clinical = read_csv('%s/data/tcga_clinical.csv' % wd, index_col=0).dropna(subset=['time', 'status'])
+clinical.index = ['%s-01' % i for i in clinical.index]
+print 'clinical', clinical.shape
 
 # Proteomics complex activities
 c_prot = read_csv('%s/tables/protein_complexes_proteomics_activities.tsv' % wd, sep='\t')
 c_prot = pivot_table(c_prot, index='complex', columns='sample', values='z')
 print 'c_prot', c_prot.shape
 
-# Transcriptomics complex activities
-c_trans = read_csv('%s/tables/protein_complexes_transcriptomics_activities.tsv' % wd, sep='\t')
-c_trans = pivot_table(c_trans, index='complex', columns='sample', values='z')
-print 'c_trans', c_trans.shape
 
-# Clinical data
-clinical = read_csv('%s/data/clinical_data.tsv' % wd, sep='\t', index_col=0).dropna(subset=['VITAL_STATUS', 'DAYS_TO_LAST_FOLLOWUP'])
-clinical['VITAL_STATUS'] = [0 if i == 'Alive' else 1 for i in clinical['VITAL_STATUS']]
-clinical = clinical[clinical['DAYS_TO_LAST_FOLLOWUP'] > 1]
-clinical = clinical.T
-print 'clinical', clinical.shape
+# -- Complexes proteins
+uniprot = read_uniprot_genename()
+corum = {k: {uniprot[p][0] for p in v if p in uniprot} for k, v in get_complexes_dict().items()}
+corum = {k: corum[k] for k in corum if len(corum[k]) > 1}
+corum_n = get_complexes_name()
+print 'corum', len(corum)
 
 
 # -- Overlap
-samples = set(c_prot).intersection(c_trans).intersection(clinical)
-complexes = set(c_prot.index).intersection(c_trans.index)
-c_prot, c_trans, clinical = c_prot.ix[complexes, samples].T, c_trans.ix[complexes, samples].T, clinical.ix[:, samples].T
-print 'c_prot', 'c_trans', 'clinical', c_prot.shape, c_trans.shape, clinical.shape
+samples = set(cnv).intersection(clinical.index).intersection(transcriptomics).intersection(proteomics)
+genes = set(cnv.index).intersection(transcriptomics.index).intersection(proteomics.index)
+print 'samples', 'genes', len(samples), len(genes)
 
 
-# -- Impute missing values with mean
-c_prot = DataFrame(Imputer().fit_transform(c_prot), index=c_prot.index, columns=c_prot.columns)
+# Residuals
+def protein_residual(g):
+    y = proteomics.ix[g, samples].dropna()
+    x = transcriptomics.ix[[g], y.index].T
 
-# -- Append clinical information
-c_prot = concat([c_prot, clinical[['VITAL_STATUS', 'DAYS_TO_LAST_FOLLOWUP']]], axis=1)
-c_trans = concat([c_trans, clinical[['VITAL_STATUS', 'DAYS_TO_LAST_FOLLOWUP']]], axis=1)
+    lm = LinearRegression().fit(x, y)
+    y_ = y - lm.coef_[0] * x[g] - lm.intercept_
+
+    return y_
+residuals = DataFrame({g: protein_residual(g) for g in genes}).T
+print 'residuals', residuals.shape
 
 
-# -- Hazard proportional regression
-# c = 285
-def complex_survival(c):
-    t_cf = CoxPHFitter(normalize=False)
-    t_cf.fit(c_trans[[c, 'DAYS_TO_LAST_FOLLOWUP', 'VITAL_STATUS']], 'DAYS_TO_LAST_FOLLOWUP', event_col='VITAL_STATUS')
-    t_cindex = concordance_index(c_trans['DAYS_TO_LAST_FOLLOWUP'], -t_cf.predict_partial_hazard(c_trans[[c]]).values.ravel(), c_trans['VITAL_STATUS'])
-    # print t_cf.print_summary()
+# -- Signif Px -> Py associations
+ppairs = read_csv('./tables/ppairs_cnv_regulation.csv')
+print ppairs
 
-    p_cf = CoxPHFitter(normalize=False)
-    p_cf.fit(c_prot[[c, 'DAYS_TO_LAST_FOLLOWUP', 'VITAL_STATUS']], 'DAYS_TO_LAST_FOLLOWUP', event_col='VITAL_STATUS')
-    p_cindex = concordance_index(c_prot['DAYS_TO_LAST_FOLLOWUP'], -t_cf.predict_partial_hazard(c_prot[[c]]).values.ravel(), c_prot['VITAL_STATUS'])
-    # print p_cf.print_summary()
 
-    res = {'complex': c, 'p_cindex': p_cindex, 't_cindex': t_cindex}
-    print res
+# px, py = 'COG3', 'COG2'
+def survival_regression(px, py):
+    df = {}
+    for p in {p for c in corum if px in corum[c] and py in corum[c] for p in corum[c]}:
+        if p in proteomics.index:
+            x = proteomics.ix[p, samples].dropna()
+            p25, p75 = np.percentile(x, 25), np.percentile(x, 75)
+            df[p] = x.map(lambda i: 1 if i > p75 else (-1 if i < p25 else 0))
 
+    df = DataFrame(df).mode(axis=1).apply(lambda x: min(x.min(), x.max(), key=abs), axis=1)
+    df = concat([df, clinical.ix[samples, ['time', 'status']]], axis=1).dropna()
+
+    cf = CoxPHFitter(normalize=False)
+    cf.fit(df, 'time', event_col='status')
+    print cf.print_summary()
+
+    cindex = concordance_index(df['time'], -cf.predict_partial_hazard(df[[0]]).values.ravel(), df['status'])
+
+    res = {'px': px, 'py': py, 'cindex': cindex}
     return res
 
-c_survival = DataFrame([complex_survival(c) for c in complexes])
-print c_survival.sort('p_cindex')
+c_survival = DataFrame([survival_regression(px, py) for px, py in ppairs[['px', 'py']].values])
+print c_survival.sort('cindex')
 
 
-# -- Plot
-sns.set(style='ticks', font_scale=.75, rc={'axes.linewidth': .3, 'xtick.major.width': .3, 'ytick.major.width': .3, 'xtick.direction': 'in', 'ytick.direction': 'in'})
+px, py = c_survival.sort('cindex').tail(1)[['px', 'py']].values[0]
 
-plt.scatter(c_survival['t_cindex'], c_survival['p_cindex'])
-plt.plot([0, 1], [0, 1], 'k--', lw=.3)
-# sns.despine(trim=True)
-plt.legend(loc='lower right')
-plt.ylabel('Proteomics ~ Survival')
-plt.xlabel('Transcriptomics ~ Survival')
-plt.xlim(.4, .7)
-plt.ylim(.4, .7)
-plt.gcf().set_size_inches(4, 4)
-plt.savefig('%s/reports/complex_survival_regression_scatter.pdf' % wd, bbox_inches='tight')
+
+df = {}
+for p in {p for c in corum if px in corum[c] and py in corum[c] for p in corum[c]}:
+    if p in proteomics.index:
+        x = proteomics.ix[p, samples].dropna()
+        p25, p75 = np.percentile(x, 25), np.percentile(x, 75)
+        df[p] = x.map(lambda i: 1 if i > p75 else (-1 if i < p25 else 0))
+
+df = DataFrame(df).mode(axis=1).apply(lambda x: min(x.min(), x.max(), key=abs), axis=1)
+df = concat([df, clinical.ix[samples, ['time', 'status']]], axis=1).dropna()
+
+samples_up = set(df[df[0] == 1].index)
+samples_down = set(df[df[0] == -1].index)
+
+logrank = logrank_test(
+    df.ix[samples_up, 'time'], df.ix[samples_down, 'time'],
+    df.ix[samples_up, 'status'], df.ix[samples_down, 'status']
+)
+
+
+sns.set(style='ticks', context='paper', rc={'axes.linewidth': .3, 'xtick.major.width': .3, 'ytick.major.width': .0, 'xtick.direction': 'out', 'ytick.direction': 'out'}, font_scale=0.75)
+ax = plt.subplot(111)
+
+kmf = KaplanMeierFitter()
+
+kmf.fit(df.ix[samples_up, 'time'], event_observed=df.ix[samples_up, 'status'], label='Up')
+kmf.plot(ci_force_lines=False, color=palette_cnv_number[1], ax=ax, show_censors=True, censor_styles={'ms': 5})
+
+kmf.fit(df.ix[samples_down, 'time'], event_observed=df.ix[samples_down, 'status'], label='Down')
+kmf.plot(ci_force_lines=False, color=palette_cnv_number[-1], ax=ax, show_censors=True, censor_styles={'ms': 5})
+
+sns.despine(ax=ax)
+
+ax.set_title('Logrank test: %.2e' % (logrank.p_value))
+ax.set_xlabel('Timeline (days)')
+ax.set_ylabel('Survival fraction')
+
+plt.gcf().set_size_inches(4, 3)
+plt.savefig('%s/reports/survival_protein_complex.pdf' % wd, bbox_inches='tight')
 plt.close('all')
-print '[INFO] Plot done'
+print '[INFO] Done'
