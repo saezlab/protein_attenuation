d126a67b3e9eb13b7762dc85318d0638534c6c00
Emanuel Goncalves <emanuelvgoncalves@gmail.com>
2016-08-25 15:30:36 +0100

    Survival analysis minor updates

--- src/cptac/survival/survival_hgsc.py
+++ src/cptac/survival/survival_hgsc.py
@@ -8,7 +8,7 @@ from lifelines import CoxPHFitter
 from lifelines import KaplanMeierFitter
 from lifelines.utils import concordance_index, k_fold_cross_validation
 from lifelines.statistics import logrank_test
-from sklearn.cross_validation import ShuffleSplit
+from sklearn.cross_validation import ShuffleSplit, KFold
 from sklearn.linear_model.base import LinearRegression
 from statsmodels.duration.hazard_regression import PHReg
 from pymist.utils.map_peptide_sequence import read_uniprot_genename
@@ -42,20 +42,10 @@ proteomics = DataFrame({i: proteomics.loc[:, [i]].mean(1) for i in set(proteomic
 # Drop missing values
 proteomics = proteomics.dropna()
 
-# Normalise
-proteomics = DataFrame({i: gkn(proteomics.ix[i].dropna()).to_dict() for i in proteomics.index}).T
-proteomics.to_csv('./data/hgsc_proteomics_processed_normalised_no_nan.csv')
-proteomics = read_csv('./data/hgsc_proteomics_processed_normalised_no_nan.csv', index_col=0)
-print 'proteomics', proteomics.shape
-
-
-# -- Overlap
+# Overlap
 proteins, samples = list(set(proteomics.index)), list(set(proteomics).intersection(clinical.index))
-print 'proteins', 'samples', len(proteins), len(samples)
 
-
-# -- Regress-out age
-# p = 'EIF3E'
+# Regress-out age, p = 'EIF3E'
 def rm_batch(p):
     ys = proteomics.ix[p, samples].dropna()
     xs = clinical.ix[ys.index, ['age']]
@@ -65,7 +55,14 @@ def rm_batch(p):
     return ys - xs.dot(lm.coef_) - lm.intercept_
 
 proteomics = DataFrame({p: rm_batch(p) for p in proteins}).T
-print '[INFO] Covariates regressed-out'
+
+# Normalise
+proteomics = DataFrame({i: gkn(proteomics.ix[i].dropna()).to_dict() for i in proteins}).T
+
+# Export
+proteomics.to_csv('./data/hgsc_proteomics_processed_normalised_no_nan.csv')
+# proteomics = read_csv('./data/hgsc_proteomics_processed_normalised_no_nan.csv', index_col=0)
+print 'proteomics', proteomics.shape
 
 
 # -- Protein complexes
@@ -83,7 +80,7 @@ print 'p_complexes', len(p_complexes)
 
 
 # --
-# c = 1097
+# px, c = 'EIF3A', 1097
 c_survival = {}
 for px, c in p_complexes:
     c_proteins = list(corum[c])
@@ -93,77 +90,80 @@ for px, c in p_complexes:
     df = concat([proteomics.ix[c_proteins].T, clinical[['time', 'status']]], axis=1).dropna()
 
     # Bootstrap
-    cv = ShuffleSplit(len(df), test_size=.3, n_iter=20)
+    # cv = ShuffleSplit(len(df), test_size=.1, n_iter=20)
+    cv = KFold(len(df), n_folds=5)
 
-    cindexes_train, cindexes_test = [], []
+    cindexes_train, cindexes_test, group = [], [], []
     for train, test in cv:
         x_train, x_test = df.ix[train], df.ix[test]
 
-        # -- Statsmodels
-        # Proportional Hazard with L1 regularisation
-        model = PHReg(x_train['time'], x_train[c_proteins], x_train['status'], ties='efron')
-        model = model.fit_regularized(L1_wt=0, alpha=.1)
+        # # -- Statsmodels
+        # # Train
+        # model = PHReg(x_train['time'], x_train[c_proteins], x_train['status'], ties='efron')
+        # model = model.fit_regularized(L1_wt=0, alpha=.1)
+        # # print model.summary()
+        #
+        # # Predict
+        # hazards_train = Series(dict(zip(*(x_train.index, model.predict(exog=x_train[c_proteins], pred_type='hr').predicted_values))))
+        # hazards_test = Series(dict(zip(*(x_test.index, model.predict(exog=x_test[c_proteins], pred_type='hr').predicted_values))))
 
-        # Predict hazards
-        hazards_train = Series(dict(zip(*(x_train.index, model.predict(exog=x_train[c_proteins], pred_type='hr').predicted_values))))
-        hazards_test = Series(dict(zip(*(x_test.index, model.predict(exog=x_test[c_proteins], pred_type='hr').predicted_values))))
+        # -- LifeLines
+        # Train
+        model = CoxPHFitter(normalize=False, penalizer=1.)
+        model = model.fit(x_train, 'time', event_col='status')
+        # print model.print_summary()
 
+        # Predict
+        hazards_train = model.predict_partial_hazard(x_train[c_proteins])[0]
+        hazards_test = model.predict_partial_hazard(x_test[c_proteins])[0]
+
+        # -- Evalute
         # Concordance index (c-index)
         cindex_train = concordance_index(x_train['time'], -hazards_train, x_train['status'])
         cindex_test = concordance_index(x_test['time'], -hazards_test, x_test['status'])
 
-        # # -- LifeLines
-        # model = CoxPHFitter(normalize=False, penalizer=.1)
-        # model = model.fit(x_train, 'time', event_col='status')
-        # # print model.print_summary()
+        # Classify into groups
+        groups_train = hazards_train.apply(lambda i: 1 if i > np.percentile(hazards_train, 50) else -1)
+        groups_test = hazards_test.apply(lambda i: 1 if i > np.percentile(hazards_test, 50) else -1)
+
+        # # Log-rank test
+        # rank_train = logrank_test(
+        #     x_train.ix[groups_train[groups_train == 1].index, 'time'], x_train.ix[groups_train[groups_train == -1].index, 'time'],
+        #     x_train.ix[groups_train[groups_train == 1].index, 'status'], x_train.ix[groups_train[groups_train == -1].index, 'status']
+        # )
         #
-        # # Predict hazards
-        # hazards_train = model.predict_partial_hazard(x_train[c_proteins])
-        # hazards_test = model.predict_partial_hazard(x_test[c_proteins])
-        #
-        # # Concordance index (c-index)
-        # cindex_train = concordance_index(x_train['time'], -hazards_train, x_train['status'])
-        # cindex_test = concordance_index(x_test['time'], -hazards_test, x_test['status'])
+        # rank_test = logrank_test(
+        #     x_test.ix[groups_test[groups_test == 1].index, 'time'], x_test.ix[groups_test[groups_test == -1].index, 'time'],
+        #     x_test.ix[groups_test[groups_test == 1].index, 'status'], x_test.ix[groups_test[groups_test == -1].index, 'status']
+        # )
 
         # -- Store results
+        group.append(groups_test)
         cindexes_train.append(cindex_train)
         cindexes_test.append(cindex_test)
-        # # print 'cindex: train %.2f, test %.2f' % (cindex_train, cindex_test)
 
-    print 'mean cindex: train %.2f, test %.2f' % (np.mean(cindexes_train), np.mean(cindexes_test))
+        # print 'cindex: train %.2f, test %.2f' % (cindex_train, cindex_test)
+        # print 'logrank: train %.2e, test %.2f' % (rank_train.p_value, rank_test.p_value)
+
+    cindexes_train_mean, cindexes_test_mean = np.median(cindexes_train), np.median(cindexes_test)
+    print 'train %.2f, test %.2f\n' % (cindexes_train_mean, cindexes_test_mean)
 
     c_survival[c] = {
-        'complex': c, 'px': px, 'len': len(c_proteins),
+        'complex': c, 'len': len(c_proteins), 'group': concat(group),
         'cindexes_train': cindexes_train, 'cindexes_test': cindexes_test,
-        'cindexes_train_mean': np.mean(cindexes_train), 'cindexes_test_mean': np.mean(cindexes_test)
+        'cindexes_train_mean': cindexes_train_mean, 'cindexes_test_mean': cindexes_test_mean
     }
 
 c_survival = DataFrame(c_survival).T
 c_survival['name'] = [corum_n[i] for i in c_survival.index]
-print c_survival.sort('cindexes_test_mean', ascending=False)[['cindexes_train_mean', 'cindexes_test_mean', 'name', 'len', 'px']]
+print c_survival.sort('cindexes_test_mean', ascending=False)[['cindexes_train_mean', 'cindexes_test_mean', 'name', 'len']]
 
 
 # -- Plot
-c = 1097
-c_proteins = list(corum[c])
+c = 811
 
-df = concat([proteomics.ix[c_proteins].T, clinical[['time', 'status']]], axis=1).dropna()
-
-# Statsmodels
-model = PHReg(df['time'], df[c_proteins], df['status'], ties='efron')
-model = model.fit()
-
-df = Series(dict(zip(*(df.index, model.predict(exog=df[c_proteins], pred_type='hr').predicted_values))))
-df = df.map(lambda i: 1 if i > np.percentile(df, 50) else -1)
+df = c_survival.ix[c, 'group']
 df = concat([df, clinical[['time', 'status']]], axis=1).dropna()
-
-# # LifeLines
-# model = CoxPHFitter(normalize=False, penalizer=.1)
-# model = model.fit(df, 'time', event_col='status')
-#
-# df = model.predict_partial_hazard(df[c_proteins])[0]
-# df = df.map(lambda i: 1 if i > np.percentile(df, 50) else -1)
-# df = concat([df, clinical[['time', 'status']]], axis=1).dropna()
 
 samples_up = set(df[df[0] == 1].index)
 samples_down = set(df[df[0] == -1].index)
